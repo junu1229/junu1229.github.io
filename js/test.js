76683ad63c22500b111d4c4f827f33a35e01869b
junu1229 <junu1229@gmail.com>
2023-08-01 19:38:49 +0900

    feat: 자석 기능 제거 및 자바스크립트 구현

--- js/test.js
+++ js/test.js
@@ -1,7 +1,12 @@
 const leftNav = document.querySelector('#leftNav');
 const mouseButton = document.querySelector('#mouseButton0');
+const cMark = document.querySelector('#cMark');
+const leftNav__p = document.querySelector('#leftNav__p p');
+const leftNav__p__name1 = document.querySelector('#leftNav__p__name1');
+const leftNav__p__name2 = document.querySelector('#leftNav__p__name2');
 const container1 = document.querySelector('#container1');
 const mouseButton1 = document.querySelector('#mouseButton1');
+const rightDot1 = document.querySelector('#rightDot1');
 const container2 = document.querySelector('#container2');
 const mouseButton2 = document.querySelector('#mouseButton2');
 const container3 = document.querySelector('#container3');
@@ -12,174 +17,72 @@ const rollbar = document.querySelector('#roll1');
 const introducemin__h2__1 = document.querySelector('#introducemin__h2__1');
 const introducemin__h2__2 = document.querySelector('#introducemin__h2__2');
 const introducemin__h2__3 = document.querySelector('#introducemin__h2__3');
+const contactme = document.querySelector('#contactme');
+
 // 
-leftNav.addEventListener("mousemove", function (e) {
-    const containerRect = leftNav.getBoundingClientRect();
-    const mouseX = e.clientX - containerRect.left;
-    const mouseY = e.clientY - containerRect.top;
-
-    const buttonWidth = mouseButton.offsetWidth;
-    const buttonHeight = mouseButton.offsetHeight;
-    console.log(buttonWidth);
-    console.log(buttonHeight);
-    const buttonX = mouseX - buttonWidth / 2;
-    const buttonY = mouseY - buttonHeight / 2;
-
-    const maxButtonX = containerRect.width - buttonWidth;
-    const maxButtonY = containerRect.height - buttonHeight;
-    mouseButton.style.left = Math.min(Math.max(buttonX, 0), maxButtonX) + "px";
-    mouseButton.style.top = Math.min(Math.max(buttonY, 0), maxButtonY) + "px";
-    mouseButton.style.transition = "all 0.3s";
-});
-
-leftNav.addEventListener("mouseleave", function () {
-    setTimeout(function () {
-        mouseButton.style.left = "50%";
-        mouseButton.style.top = "50%";
-        mouseButton.style.transform = "translate(-50%, -50%)";
-    }, 50);
-});
-
 leftNav.addEventListener("mouseover", function () {
-    mouseButton.style.transform = "scale(1.2)";
+    cMark.style.transform = "rotateZ(360deg)";
+    cMark.style.transition = "all 1.2s";
+    leftNav__p.style.transform = "translateX(-5.1rem)";
+    leftNav__p.style.fontSize = "1.25rem";
+    leftNav__p.style.transition = "all 1.2s";
+    leftNav__p__name1.style.transform = "translateX(-5.1rem)";
+    leftNav__p__name1.style.transition = "all 1.2s";
+    leftNav__p__name2.style.transform = "translateX(-5.1rem)";
+    leftNav__p__name2.style.transition = "all 1.2s";
 });
-// container1 mouseButton1
-container1.addEventListener("mousemove", function (e) {
-    const containerRect = container1.getBoundingClientRect();
-    const mouseX = e.clientX - containerRect.left;
-    const mouseY = e.clientY - containerRect.top;
-
-    const buttonWidth = mouseButton1.offsetWidth;
-    const buttonHeight = mouseButton1.offsetHeight;
-    const buttonX = mouseX - buttonWidth / 2;
-    const buttonY = mouseY - buttonHeight / 2;
-
-    const maxButtonX = containerRect.width - buttonWidth;
-    const maxButtonY = containerRect.height - buttonHeight;
-    mouseButton1.style.left = Math.min(Math.max(buttonX, 0), maxButtonX) + "px";
-    mouseButton1.style.top = Math.min(Math.max(buttonY, 0), maxButtonY) + "px";
-    mouseButton1.style.transition = "all 0.3s";
+leftNav.addEventListener("mouseout", function () {
+    cMark.style.transform = "rotateZ(0deg)";
+    cMark.style.transition = "all 1.2s";
+    leftNav__p.style.transform = "translateX(0)";
+    leftNav__p.style.transition = "all 1.2s";
+    leftNav__p.style.fontSize = "1.3rem";
+    leftNav__p__name1.style.transform = "translateX(0)";
+    leftNav__p__name1.style.transition = "all 1.2s";
+    leftNav__p__name2.style.transform = "translateX(0)";
+    leftNav__p__name2.style.transition = "all 1.2s";
 });
-
-container1.addEventListener("mouseleave", function () {
-    setTimeout(function () {
-        mouseButton1.style.left = "50%";
-        mouseButton1.style.top = "50%";
-        mouseButton1.style.transform = "translate(-50%, -50%)";
-    }, 50);
-});
-
-container1.addEventListener("mouseover", function () {
-    mouseButton1.style.transform = "scale(1.2)";
-});
-// container2 mouseButton2
-container2.addEventListener("mousemove", function (e) {
-    const containerRect = container2.getBoundingClientRect();
-    const mouseX = e.clientX - containerRect.left;
-    const mouseY = e.clientY - containerRect.top;
-
-    const buttonWidth = mouseButton2.offsetWidth;
-    const buttonHeight = mouseButton2.offsetHeight;
-    const buttonX = mouseX - buttonWidth / 2;
-    const buttonY = mouseY - buttonHeight / 2;
-
-    const maxButtonX = containerRect.width - buttonWidth;
-    const maxButtonY = containerRect.height - buttonHeight;
-    mouseButton2.style.left = Math.min(Math.max(buttonX, 0), maxButtonX) + "px";
-    mouseButton2.style.top = Math.min(Math.max(buttonY, 0), maxButtonY) + "px";
-    mouseButton2.style.transition = "all 0.3s";
-});
-
-container2.addEventListener("mouseleave", function () {
-    setTimeout(function () {
-        mouseButton2.style.left = "50%";
-        mouseButton2.style.top = "50%";
-        mouseButton2.style.transform = "translate(-50%, -50%)";
-    }, 50);
-});
-
-container2.addEventListener("mouseover", function () {
-    mouseButton2.style.transform = "scale(1.2)";
-});
-// container3 mouseButton3
-container3.addEventListener("mousemove", function (e) {
-    const containerRect = container3.getBoundingClientRect();
-    const mouseX = e.clientX - containerRect.left;
-    const mouseY = e.clientY - containerRect.top;
-
-    const buttonWidth = mouseButton3.offsetWidth;
-    const buttonHeight = mouseButton3.offsetHeight;
-    const buttonX = mouseX - buttonWidth / 2;
-    const buttonY = mouseY - buttonHeight / 2;
-
-    const maxButtonX = containerRect.width - buttonWidth;
-    const maxButtonY = containerRect.height - buttonHeight;
-    mouseButton3.style.left = Math.min(Math.max(buttonX, 0), maxButtonX) + "px";
-    mouseButton3.style.top = Math.min(Math.max(buttonY, 0), maxButtonY) + "px";
-    mouseButton3.style.transition = "all 0.3s";
-});
-
-container3.addEventListener("mouseleave", function () {
-    setTimeout(function () {
-        mouseButton3.style.left = "50%";
-        mouseButton3.style.top = "50%";
-        mouseButton3.style.transform = "translate(-50%, -50%)";
-    }, 50);
-});
-
-container3.addEventListener("mouseover", function () {
-    mouseButton3.style.transform = "scale(1.2)";
-});
-// ballContainer aBall
-ballContainer.addEventListener("mousemove", function (e) {
-    const containerRect = ballContainer.getBoundingClientRect();
-    const mouseX = e.clientX - containerRect.left;
-    const mouseY = e.clientY - containerRect.top;
-
-    const buttonWidth = aBall.offsetWidth;
-    const buttonHeight = aBall.offsetHeight;
-    const buttonX = mouseX - buttonWidth / 2;
-    const buttonY = mouseY - buttonHeight / 2;
-
-    const maxButtonX = containerRect.width - buttonWidth;
-    const maxButtonY = containerRect.height - buttonHeight;
-    aBall.style.left = Math.min(Math.max(buttonX, 0), maxButtonX) + "px";
-    aBall.style.top = Math.min(Math.max(buttonY, 0), maxButtonY) + "px";
-    aBall.style.transition = "all 0.3s";
-});
-ballContainer.addEventListener("mouseleave", function () {
-    setTimeout(function () {
-        aBall.style.left = "50%";
-        aBall.style.top = "50%";
-        aBall.style.transform = "translate(-50%, -50%)";
-    }, 0);
-});
-
-ballContainer.addEventListener("mouseleave", function () {
-    aBall.style.backgroundColor = "black";
-    aBall.style.transition = "all 0.7s";
-});
-
-ballContainer.addEventListener("mouseover", function () {
-    aBall.style.transform = "scale(1.2)";
-});
-ballContainer.addEventListener("mouseover", function () {
+aBall.addEventListener("mouseover", function () {
     aBall.style.backgroundColor = "#4B89DC";
     aBall.style.transition = "all 0.7s";
 });
-
+aBall.addEventListener("mouseout", function () {
+    aBall.style.backgroundColor = "black";
+    aBall.style.transition = "all 0.7s";
+});
+container1.addEventListener("mouseover", function () {
+    mouseButton1.style.transform = "translateY(-0.5rem)";
+    mouseButton1.style.transition = "0.7s";
+});
+container1.addEventListener("mouseout", function () {
+    mouseButton1.style.transform = "translateY(0)";
+    mouseButton1.style.transition = "0.7s";
+});
+container2.addEventListener("mouseover", function () {
+    mouseButton2.style.transform = "translateY(-0.5rem)";
+    mouseButton2.style.transition = "0.7s";
+});
+container2.addEventListener("mouseout", function () {
+    mouseButton2.style.transform = "translateY(0)";
+    mouseButton2.style.transition = "0.7s";
+});
+container3.addEventListener("mouseover", function () {
+    mouseButton3.style.transform = "translateY(-0.5rem)";
+    mouseButton3.style.transition = "0.7s";
+});
+container3.addEventListener("mouseout", function () {
+    mouseButton3.style.transform = "translateY(0)";
+    mouseButton3.style.transition = "0.7s";
+});
 window.addEventListener('scroll', function(){
     // console.log(window.pageXOffset); //deprecated
     // console.log(this.window.scrollY);
     // console.log(this.document.body.offsetHeight)
     // console.log(this.window.innerHeight);
-
     let width = ((this.window.scrollY/(this.document.body.offsetHeight-this.window.innerHeight))*100);
-    let rollbarwidth = -((this.window.scrollY/(this.document.body.offsetHeight-this.window.innerHeight))*50)+35;
     let ballContainerwidth = -((this.window.scrollY/(this.document.body.offsetHeight-this.window.innerHeight))*100)+40;
-    rollbar.style.top = rollbarwidth + "%";
     ballContainer.style.top = ballContainerwidth + "%";
-    console.log(width);
+    // console.log(width);
     if(width>=17.5){
         introducemin__h2__1.style.opacity = "100%";
     } else {
@@ -195,5 +98,28 @@ window.addEventListener('scroll', function(){
     } else {
         introducemin__h2__3.style.opacity = "0%";
     }
-
+    if(width>=79.5) {
+        contactme.style.opacity = "100%";
+        contactme.style.transition = "0.7s";
+    } else {
+        contactme.style.opacity = "0%";
+        contactme.style.transition = "0.7s";
+    }
 });
+const rollbarMatrix = window.getComputedStyle(rollbar).transform;
+window.addEventListener('wheel', function(e){
+    console.log(e.deltaY);
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+    let i = 0;
+    if(e.deltaY>=0) {
+            rollbar.style.transform = "translateX(50%)";
+            rollbar.style.transition = "40s linear";
+    } else if(e.deltaY<0) {
+            rollbar.style.transform = "translateX(-50%)";
+            rollbar.style.transition = "40s linear";
+    }
+})
+window.addEventListener('DOMContentLoaded', function(e){
+    rollbar.style.transform = "translateX(50%)";
+    rollbar.style.transition = "40s linear";
+})
